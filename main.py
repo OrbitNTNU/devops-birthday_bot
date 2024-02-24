a6f8fe75d08a13c195de4863472f434ea47d5e8b
AndreasTauge <andreas@tauge.no>
2024-02-25 00:07:11 +0100

    Simplified user authorization with service account

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from google.oauth2 import service_account
 import datetime 
 from slack_message import slack_send_message
 
@@ -18,19 +19,11 @@ SAMPLE_RANGE_NAME = "A:C"
 
 def main():
   creds = None
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=0)
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
 
+  SERVICE_ACCOUNT_FILE = "service_account.json"
+  creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+  
   try:
     service = build("sheets", "v4", credentials=creds)
 
